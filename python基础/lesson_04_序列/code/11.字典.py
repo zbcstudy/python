e4ba46bdff1a48c6d18f794443bde64a72e59624
arron.zhao <aaron.zhao>
2022-07-08 16:54:18 +0800

    format

--- python基础/lesson_04_序列/code/11.字典.py
+++ python基础/lesson_04_序列/code/11.字典.py
@@ -1,6 +1,6 @@
 # 字典
 # 使用 {} 来创建字典
-d = {} # 创建了一个空字典
+d = {}  # 创建了一个空字典
 
 # 创建一个保护有数据的字典
 # 语法：
@@ -10,16 +10,16 @@ d = {} # 创建了一个空字典
 #       字典的键是不能重复的，如果出现重复的后边的会替换到前边的
 # d = {'name':'孙悟空' , 'age':18 , 'gender':'男' , 'name':'sunwukong'}
 d = {
-'name':'孙悟空' , 
-'age':18 , 
-'gender':'男' , 
-'name':'sunwukong'
+    'name': '孙悟空',
+    'age': 18,
+    'gender': '男',
+    'name': 'sunwukong'
 }
 
 print(d, type(d))
 
 # 需要根据键来获取值
-# print(d['name'],d['age'],d['gender'])
+print(d['name'], d['age'], d['gender'])
 
 # 如果使用了字典中不存在的键，会报错
-# print(d['hello']) KeyError: 'hello'
+print(d['hello'])  # KeyError: 'hello'
